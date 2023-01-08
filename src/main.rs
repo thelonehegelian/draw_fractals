64bcb4d5f608c712bc7587a9ea53086523405197
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-08 18:02:54 +0000

    Add julia function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use image::{Rgb, RgbImage};
+use image::{ImageBuffer, Pixel, Rgb};
+use num::complex::Complex;
 
 fn main() {
-    let mut img = RgbImage::new(32, 32);
 
-    for x in 15..=17 {
-        for y in 8..24 {
-            img.put_pixel(x, y, Rgb([255, 0, 0]));
-            img.put_pixel(y, x, Rgb([255, 0, 0]));
+    // Save the image to a file or display it in some way...
+}
+
+// julia set creator
+fn julia(c: Complex<f32>, x: u32, y: u32, width: u32, height: u32, max_iter: u32) -> u32 {
+    let width = width as f32;
+    let height = height as f32;
+
+    let mut z = Complex {
+        // scale and translate the point to image coordinates
+        re: 3.0 * (x as f32 - 0.5 * width) / width,
+        im: 2.0 * (y as f32 - 0.5 * height) / height,
+    };
+    let mut i = 0;
+    for t in 0..max_iter {
+        if z.norm() >= 2.0 {
+            break;
         }
+        z = z * z + c;
+        i = t;
     }
-
-    img.save("sample.jpg");
+    i
 }
